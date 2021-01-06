c4820610a949fed678d3924d4f61a8def52cad5d
snxx-lppxx <snmgreenx@gmail.com>
2021-01-07 01:47:57 +0500

    Added main file

---
+++ Go/src/main.go
@@ -0,0 +1,17 @@
+//---==== Horder Inc ====---
+
+//====--- creator: snxx (snxx-lppxx)
+//====--- blog: https://t.me/BlogSelfMade
+//====--- git: https://github.com/snxx-lppxx
+
+//---====---====---====---====---====---====
+
+package main
+import "fmt"
+
+func main() {
+	Rich := 23 
+	Rss := 'Hello'
+	Friend := 'my friends!'
+	fmt.Println("%s %d %s", Rss, Rich, Friend)
+}
